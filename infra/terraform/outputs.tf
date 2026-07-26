f5a5748c8a774fed71d4c19c01eb1bcf55a5d11b
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 05:24:10 -0300

    fix: export processing queue identifiers

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,18 @@
+output "processing_queue_name" {
+  description = "Name of the processing source queue."
+  value       = aws_sqs_queue.processing.name
+}
+
+output "processing_queue_arn" {
+  description = "ARN of the processing source queue."
+  value       = aws_sqs_queue.processing.arn
+}
+
+output "processing_queue_url" {
+  description = "URL of the processing source queue."
+  value       = aws_sqs_queue.processing.url
+}
+
 output "processing_dlq_name" {
   description = "Name of the processing dead-letter queue."
   value       = aws_sqs_queue.processing_dlq.name
