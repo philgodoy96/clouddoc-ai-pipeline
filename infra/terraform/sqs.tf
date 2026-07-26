e0171c905601f0358dc8787ce80b5ab58f72cbf2
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 04:52:26 -0300

    feat: add processing queue redrive policy

--- infra/terraform/sqs.tf
+++ infra/terraform/sqs.tf
@@ -1,11 +1,11 @@
 resource "aws_sqs_queue" "processing_dlq" {
   name = "${local.name_prefix}-processing-dlq"
 
-  fifo_queue                  = false
-  delay_seconds               = 0
-  visibility_timeout_seconds  = 180
-  message_retention_seconds   = 1209600
-  sqs_managed_sse_enabled     = true
+  fifo_queue                 = false
+  delay_seconds              = 0
+  visibility_timeout_seconds = 180
+  message_retention_seconds  = 1209600
+  sqs_managed_sse_enabled    = true
 
   tags = {
     Name      = "${local.name_prefix}-processing-dlq"
