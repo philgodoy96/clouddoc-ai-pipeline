84765239bd2276180d1b1619d6e48e6ad6f865ee
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 11:18:36 -0300

    feat: add managed Lambda log groups

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -2,7 +2,14 @@ locals {
   name_prefix              = "${var.project_name}-${var.environment}"
   documents_bucket_name    = "${local.name_prefix}-${data.aws_caller_identity.current.account_id}-documents"
   document_jobs_table_name = "${local.name_prefix}-document-jobs"
-  is_production            = var.environment == "prod"
+
+  create_job_function_name             = "${local.name_prefix}-create-job"
+  get_job_function_name                = "${local.name_prefix}-get-job"
+  processor_function_name              = "${local.name_prefix}-process-document"
+  dead_letter_reconciler_function_name = "${local.name_prefix}-reconcile-dead-letter"
+
+  is_production             = var.environment == "prod"
+  lambda_log_retention_days = local.is_production ? 30 : 14
 
   common_tags = {
     Project     = var.project_name
