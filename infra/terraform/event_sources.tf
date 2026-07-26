0b47360282a83e4ff0d3c3d4a34340b75551496c
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 13:05:51 -0300

    feat: connect processing DLQ to reconciler Lambda

--- infra/terraform/event_sources.tf
+++ infra/terraform/event_sources.tf
@@ -18,3 +18,24 @@ resource "aws_lambda_event_source_mapping" "processing_queue" {
     aws_iam_role_policy.processor_queue_consumer,
   ]
 }
+
+resource "aws_lambda_event_source_mapping" "processing_dlq" {
+  event_source_arn = aws_sqs_queue.processing_dlq.arn
+  function_name    = aws_lambda_function.dead_letter_reconciler.arn
+  enabled          = true
+
+  batch_size                         = 1
+  maximum_batching_window_in_seconds = 0
+
+  function_response_types = [
+    "ReportBatchItemFailures",
+  ]
+
+  scaling_config {
+    maximum_concurrency = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy.dead_letter_reconciler_queue_consumer,
+  ]
+}
