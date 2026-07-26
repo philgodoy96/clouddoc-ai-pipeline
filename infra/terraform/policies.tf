f3d751ee73a0043057d853516dbc459c5aab723b
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 05:54:05 -0300

    feat: allow S3 event delivery to processing queue

--- infra/terraform/policies.tf
+++ infra/terraform/policies.tf
@@ -33,3 +33,48 @@ resource "aws_s3_bucket_policy" "documents" {
   bucket = aws_s3_bucket.documents.id
   policy = data.aws_iam_policy_document.documents_https_only.json
 }
+
+data "aws_iam_policy_document" "processing_queue_allow_s3" {
+  version = "2012-10-17"
+
+  statement {
+    sid    = "AllowDocumentBucketNotifications"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "s3.amazonaws.com",
+      ]
+    }
+
+    actions = [
+      "sqs:SendMessage",
+    ]
+
+    resources = [
+      aws_sqs_queue.processing.arn,
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values = [
+        aws_s3_bucket.documents.arn,
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values = [
+        data.aws_caller_identity.current.account_id,
+      ]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "processing_s3_publish" {
+  queue_url = aws_sqs_queue.processing.url
+  policy    = data.aws_iam_policy_document.processing_queue_allow_s3.json
+}
