573a30dd656b2c2c8dc4bd13634e72f6ecdc70d1
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 12:53:39 -0300

    feat: add reconciliation failure quarantine queue

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -38,6 +38,21 @@ output "processing_dlq_url" {
   value       = aws_sqs_queue.processing_dlq.url
 }
 
+output "reconciliation_failures_queue_name" {
+  description = "Name of the terminal dead-letter reconciliation quarantine queue."
+  value       = aws_sqs_queue.reconciliation_failures.name
+}
+
+output "reconciliation_failures_queue_arn" {
+  description = "ARN of the terminal dead-letter reconciliation quarantine queue."
+  value       = aws_sqs_queue.reconciliation_failures.arn
+}
+
+output "reconciliation_failures_queue_url" {
+  description = "URL of the terminal dead-letter reconciliation quarantine queue."
+  value       = aws_sqs_queue.reconciliation_failures.url
+}
+
 output "document_jobs_table_name" {
   description = "Name of the authoritative document-jobs table."
   value       = aws_dynamodb_table.document_jobs.name
