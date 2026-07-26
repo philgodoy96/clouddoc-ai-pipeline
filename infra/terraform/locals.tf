688e63f8ef9984f836f3e823b59cd62fc8af88d0
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 05:48:43 -0300

    feat: add private versioned document bucket

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  name_prefix = "${var.project_name}-${var.environment}"
+  name_prefix           = "${var.project_name}-${var.environment}"
+  documents_bucket_name = "${local.name_prefix}-${data.aws_caller_identity.current.account_id}-documents"
 
   common_tags = {
     Project     = var.project_name
