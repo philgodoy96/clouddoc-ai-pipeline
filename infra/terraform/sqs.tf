78a881044e925141f6faaa8e83859a40dc7d61c3
Felipe Godoy <godoyphil9@gmail.com>
2026-07-26 05:11:26 -0300

    feat: restrict processing DLQ redrive sources

--- infra/terraform/sqs.tf
+++ infra/terraform/sqs.tf
@@ -12,3 +12,34 @@ resource "aws_sqs_queue" "processing_dlq" {
     QueueRole = "processing-dead-letter"
   }
 }
+
+resource "aws_sqs_queue" "processing" {
+  name = "${local.name_prefix}-processing"
+
+  fifo_queue                 = false
+  delay_seconds              = 0
+  visibility_timeout_seconds = 180
+  message_retention_seconds  = 1209600
+  sqs_managed_sse_enabled    = true
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.processing_dlq.arn
+    maxReceiveCount     = 3
+  })
+
+  tags = {
+    Name      = "${local.name_prefix}-processing"
+    QueueRole = "processing"
+  }
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "processing_dlq" {
+  queue_url = aws_sqs_queue.processing_dlq.url
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue"
+    sourceQueueArns = [
+      aws_sqs_queue.processing.arn,
+    ]
+  })
+}
