211a1a7e76b9fc7b6b32d6ff152007fa89df2336
Felipe Godoy <godoyphil9@gmail.com>
2026-07-27 00:37:47 -0300

    feat: add document job API integrations and routes

--- infra/terraform/api_gateway.tf
+++ infra/terraform/api_gateway.tf
@@ -20,3 +20,43 @@ resource "aws_cloudwatch_log_group" "control_plane_api_access" {
     LogRole = "api-access"
   }
 }
+
+resource "aws_apigatewayv2_integration" "create_job" {
+  api_id = aws_apigatewayv2_api.control_plane.id
+
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.create_job.invoke_arn
+
+  payload_format_version = "2.0"
+  timeout_milliseconds   = 15000
+}
+
+resource "aws_apigatewayv2_integration" "get_job" {
+  api_id = aws_apigatewayv2_api.control_plane.id
+
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.get_job.invoke_arn
+
+  payload_format_version = "2.0"
+  timeout_milliseconds   = 10000
+}
+
+resource "aws_apigatewayv2_route" "create_job" {
+  api_id = aws_apigatewayv2_api.control_plane.id
+
+  route_key          = "POST /v1/document-jobs"
+  authorization_type = "AWS_IAM"
+
+  target = "integrations/${aws_apigatewayv2_integration.create_job.id}"
+}
+
+resource "aws_apigatewayv2_route" "get_job" {
+  api_id = aws_apigatewayv2_api.control_plane.id
+
+  route_key          = "GET /v1/document-jobs/{job_id}"
+  authorization_type = "AWS_IAM"
+
+  target = "integrations/${aws_apigatewayv2_integration.get_job.id}"
+}
